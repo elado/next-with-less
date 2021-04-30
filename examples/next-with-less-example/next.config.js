6737bc3481be79d4ab2a48b2f30bec460e68639a
elado <elad.ossadon@databricks.com>
2021-04-30 16:15:10 -0700

    example update

--- examples/next-with-less-example/next.config.js
+++ examples/next-with-less-example/next.config.js
@@ -1,9 +1,21 @@
 // outside of this repo, this should be:
 // const withLess = require("next-with-less");
-const withLess = require("../../");
+const withLess = require("../../src");
 
 module.exports = withLess({
   future: {
     webpack5: true,
+  },
+
+  lessLoaderOptions: {
+    // it's possible to use additionalData or modifyVars for antd theming
+    // read more @ https://ant.design/docs/react/customize-theme
+    additionalData: (content) => `${content}\n@border-radius-base: 10px;`,
+
+    lessOptions: {
+      modifyVars: {
+        "primary-color": "#9900FF",
+      },
+    },
   },
 });
