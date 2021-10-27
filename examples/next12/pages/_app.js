b85522e6b8beae767e361ba026323ee4d1c34b6f
Elad Ossadon <elad@ossadon.com>
2021-10-26 21:07:30 -0700

    Next 12 support (#13)

---
+++ examples/next12/pages/_app.js
@@ -0,0 +1,10 @@
+import "antd/dist/antd.less";
+
+import "../styles/globals.css";
+import "../styles/globals.less";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
